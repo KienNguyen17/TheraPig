abb9bf13ea16c77b8899ce7a3208d8f8e3fb54e0
Kien Nguyen <knguyen2@macalester.edu>
2024-03-05 01:04:57 -0600

    minigame newspaper

---
+++ Assets/Scripts/paperController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperController : MonoBehaviour
+{
+    public bool isClicked;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClicked = false;
+        if (isClicked) {
+            Debug.Log("initialized");
+        }
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDrag()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+        transform.position = new Vector2(worldPos.x, worldPos.y);
+    }
+}
