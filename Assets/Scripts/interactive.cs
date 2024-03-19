abe0df41d9e53bc309c913520d6cb1a917936ae1
Kien Nguyen <knguyen2@macalester.edu>
2024-03-19 11:11:41 -0500

    dialguerunner running

---
+++ Assets/Scripts/interactive.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Yarn.Unity;
+
+public class interactive : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    public bool isInteractible;
+    DialogueRunner runner;
+    public UnityEvent intoInventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        runner = FindObjectOfType<DialogueRunner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q)) {
+            Debug.Log("Q has been clicked");
+            PickedUp();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        renderer.color = new Color(1,1,1,0.3f);
+        isInteractible = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        renderer.color = new Color(1,1,1,1);
+        isInteractible = false;
+    }
+
+    void PickedUp() {
+        if (isInteractible) {
+            isInteractible = false;
+            runner.StartDialogue("secondBadEnd");
+            intoInventory.Invoke();
+            renderer.enabled = false;
+        }
+    }
+}
