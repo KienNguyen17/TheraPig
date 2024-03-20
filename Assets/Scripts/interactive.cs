5c415798a5f677cc1166a29609488d215497564f
Kien Nguyen <knguyen2@macalester.edu>
2024-03-20 18:33:38 -0500

    Some inventory stuffs

--- Assets/Scripts/interactive.cs
+++ Assets/Scripts/interactive.cs
@@ -9,21 +9,18 @@ public class interactive : MonoBehaviour
     SpriteRenderer renderer;
     public bool isInteractible;
     public UnityEvent triggerDialogue;
-    // DialogueRunner runner;
-    // public UnityEvent intoInventory;
+    public UnityEvent intoInventory;
 
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
-        // runner = FindObjectOfType<DialogueRunner>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Q)) {
-            Debug.Log("Q has been clicked");
             PickedUp();
         }
     }
@@ -41,9 +38,13 @@ public class interactive : MonoBehaviour
     void PickedUp() {
         if (isInteractible) {
             isInteractible = false;
-            // runner.StartDialogue("Start");
-            triggerDialogue.Invoke();
+            // triggerDialogue.Invoke();
+            intoInventory.Invoke();
             renderer.enabled = false;
         }
+    }
+
+    void Dropped() {
+        //TODO: add a way to drop the object
     }
 }
