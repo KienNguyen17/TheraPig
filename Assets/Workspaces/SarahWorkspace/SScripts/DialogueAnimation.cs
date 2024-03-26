3ea83c7add0b8b25f857a98ce8dcbc9d54e992b7
Sarah Sylvester <ssylvest@macalester.edu>
2024-03-26 16:36:47 -0500

    Added inventory add / drop component to main

--- Assets/Workspaces/SarahWorkspace/SScripts/DialogueAnimation.cs
+++ Assets/Workspaces/SarahWorkspace/SScripts/DialogueAnimation.cs
@@ -7,13 +7,15 @@ using Yarn.Unity;
 
 public class DialogueAnimation : MonoBehaviour
 {
-    public UnityEvent startAnimation, secondAnimation, endAnimation;
+    public UnityEvent startAnimation, endAnimation;
     public GameObject timeLine;
     public GameObject player;
     PlayableDirector director;
+    bool qPressed;
+    bool animationPlayedAlready = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent triggerDialogue;
+void Start()
     {
         director = timeLine.GetComponent<PlayableDirector>();
         director.stopped += OnPlayableDirector;
@@ -21,26 +23,29 @@ public class DialogueAnimation : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        // User input == Q
-            // play the scenes and dialogue
+    { 
+        // how to get it so the user can only see this particular animation once
+        if (!animationPlayedAlready && qPressed && Input.GetKey(KeyCode.Q)) {
+            AnimationEnter();
+            triggerDialogue.Invoke();
+            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("You hit THE duck.");
+        qPressed = true;
     }
 
     void OnPlayableDirector(PlayableDirector direct) {
         endAnimation.Invoke(); 
     }
 
-    [YarnCommand("animate")]
-    void StartFirstAnimation() {
-        // if (Input.GetKey(KeyCode.Q)) {
-            startAnimation.Invoke();
-            director.Play();    
-        // }
+    // Have the animation play first, then when the animation is done, play the next dialogue
+    void AnimationEnter() {
+        startAnimation.Invoke();
+        director.Play();
+        animationPlayedAlready = true;
     }
 
-    // [YarnCommand("animate1")]
-    // void StartSecondAnimation() {
-    //     secondAnimation.Invoke();
-    //     director.Play();
-    // }
 }
