26f7352a037ea5f73080d22a4515c6a81630a66d
Kien Nguyen <knguyen2@macalester.edu>
2024-03-28 11:18:06 -0500

    drop logic

--- Assets/Scripts/newspaperDrop.cs
+++ Assets/Scripts/newspaperDrop.cs
@@ -7,10 +7,14 @@ public class newspaperDrop : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool isGoodEnd;
+    public int count;
+    Inventory inventory;
     InMemoryVariableStorage variableStorage;
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
+        inventory = GameObject.FindObjectOfType<Inventory>();
         variableStorage = GameObject.FindObjectOfType<InMemoryVariableStorage>();
         variableStorage.TryGetValue("$secondEnd", out isGoodEnd);
     }
@@ -18,19 +22,22 @@ public class newspaperDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        variableStorage.SetValue("$secondEnd", (count==2));
     }
 
     void OnTriggerStay2D(Collider2D collision) {
-        if(collision.GetComponent<Collider2D>().CompareTag("Newspaper")){
-            isGoodEnd = true;
-            variableStorage.SetValue("$secondEnd", isGoodEnd);
+        if (inventory.newItem != null){
+            count++;
+        } else {
+            count--;
         }
     }
 
-    // void OnTriggerExit2D(Collider2D collision) {
-    //     if(collision.GetComponent<Collider2D>().CompareTag("Newspaper")){
-    //         isGoodEnd = false;
-    //     }
-    // }
+    void OnTriggerExit2D(Collider2D collision) {
+        if (inventory.newItem != null){
+            count--;
+        } else {
+            count++;
+        }
+    }
 }
