f5f79c5202cc89c7535c226d86570fa8801f7c75
Kien Nguyen <knguyen2@macalester.edu>
2024-03-28 10:58:31 -0500

    yarn and newspaper stuff

--- Assets/Scripts/newspaperDrop.cs
+++ Assets/Scripts/newspaperDrop.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class newspaperDrop : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool isGoodEnd = false;
+    public bool isGoodEnd;
+    InMemoryVariableStorage variableStorage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        variableStorage = GameObject.FindObjectOfType<InMemoryVariableStorage>();
+        variableStorage.TryGetValue("$secondEnd", out isGoodEnd);
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class newspaperDrop : MonoBehaviour
     void OnTriggerStay2D(Collider2D collision) {
         if(collision.GetComponent<Collider2D>().CompareTag("Newspaper")){
             isGoodEnd = true;
+            variableStorage.SetValue("$secondEnd", isGoodEnd);
         }
     }
 
