2f95c1d2424820a66e9b2265cbce9933e22e69b3
Kien Nguyen <knguyen2@macalester.edu>
2024-04-23 10:53:47 -0500

    fade in and out

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Fader : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    bool isFading = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    [YarnCommand("fade")]
+    public void Fade() {
+        if (!isFading) {
+            renderer.color = new Color(0,0,0,1);
+            StartCoroutine(FadeIn()); 
+        }     
+    }
+
+    public IEnumerator FadeIn() {
+        isFading = true;
+        for (float i=0; i<=1; i+=0.01f) {
+            renderer.color = new Color(0,0,0,i);
+            yield return new WaitForSeconds(0.01f);
+        }
+        for (float i=1; i>0; i-=0.01f) {
+            renderer.color = new Color(0,0,0,i);
+            yield return new WaitForSeconds(0.03f);
+        }
+        isFading = false;
+    }
+}
